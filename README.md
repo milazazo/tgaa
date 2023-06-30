9886ea82a6a887f28e2c7738a54373ebde519ac9
milazazo <clayterrans+git@gmail.com>
2023-06-30 22:09:54 +1200

    sillay brothers
    Former-commit-id: fea264d287e6b215d0cba7696551ea7da117521f [formerly 85ea9847d577ff641ae2f81d3cabb8240ff95467]
    Former-commit-id: fbeb44714e7798148b63aa8b560a358f09f2932c

---
+++ README.md
@@ -0,0 +1,22 @@
+# About
+Art collaboration for The Great Ace Attorney. Live page is viewable at [demo](https://tgaa.milaza.art/demo). Planned to be complete in late July.
+
+## To-do
+- [x] **Add placeholder images**
+- [ ] **Add loading screen**
+- [ ] Add loading animation
+- [x] Make chibi characters draggable
+- [x] **Make chibi characters clickable (and add them to credits)**
+- [ ] **Update UI images**
+  - Pretty up the module buttons
+  - Update bottom-right buttons to be more visible
+- [ ] Replace mySlides with something else lol
+- [ ] Make new button/banner (no one else is going to use this)
+- [ ] **Fix mobile spacing**
+- [ ] **Add all images**
+- [x] **Add hover images and chibi images to main image modal**
+- [ ] **Optimize all images (resize, compress, convert to .webp)**
+
+## Other
+* If you have any suggestions (code-wise or other), feel free to tell me unless it's already noted in the to-do list above
+* It's optimized for desktop 1920*1080 resolution on Google Chrome. Should be fine on mobile phones as well, not great on tablets though
---
+++ array.js
@@ -0,0 +1,193 @@
+// Create the character array
+function characterArray() {
+
+    // Get data.csv and turn it into something readable
+    fetch('data.csv')
+    .then(response => response.text())
+    .then(characterCsv => {
+
+    // Read data.csv and create the array "characterArray" which creates objects for each rows in the array with these listed properties
+    const rows = characterCsv.split('\n');
+    const characterArray = rows.map(row => {
+    const values = row.split(',');
+    return {
+        id: values[0],
+        character: values[1],
+        chibi: values[2],
+        alt: values[3],
+        artist: values[4],
+        twitter: values[5],
+        tumblr: values[6],
+        instagram: values[7],
+        website: values[8],
+        websitename: values[9],
+        description: values[10]
+    };
+    });
+    console.log(characterArray);
+
+    // Get the modal slides and create as many slides as there are rows in data.csv
+    let slides = document.getElementById('slide-parent');
+      
+    for (let i = 0; i < characterArray.length; i++) {
+        let slideDiv = document.createElement('div');
+        slideDiv.className = 'mySlides';
+        slides.appendChild(slideDiv);
+    }
+
+
+    // CHARACTER ARTWORK
+
+    const charaArt = document.querySelectorAll(".chara-item");
+    const slideBox = document.querySelectorAll(".mySlides");
+    charaArt.forEach((box, index) => {
+        box.innerHTML = `<img src="art/` + characterArray[index].artist.toLowerCase() +`.webp" onclick="openModal();currentSlide(`+ [index+1] +`)">`;
+      });
+
+    // SLIDE BOXES
+
+    slideBox.forEach((box, index) => { // For each instance of the class ".mySlides" (slideBox)
+        let description = ""; // Check if there's a description
+        if (characterArray[index].hasOwnProperty("description")) {
+            description += characterArray[index].description; // If there is a description, display it
+        } else {
+            description += ``;
+        }
+
+        let title = ""; // Check if there's a chibi
+        let chibiart = "";
+        if (characterArray[index].chibi !== '') {
+            title += characterArray[index].character + " and " + characterArray[index].chibi; // If there is a chibi, display it
+            chibiart += `<img src="art/`+ characterArray[index].artist.toLowerCase() +`-sm.webp" class="chibi-img" id="modal-chibi-`+ characterArray[index].artist.toLowerCase() +`">`;
+        } else {
+            title += characterArray[index].character;
+        }
+
+        let art = ""; // Check if there's an alt art
+        if (characterArray[index].alt !== '') {
+            art += `<img src="art/`+ characterArray[index].artist.toLowerCase() +`.webp" class="front-img1"> <img src="art/`+ characterArray[index].artist.toLowerCase() +`-2.webp" class="front-img2">`
+        } else {
+            art += `<img src="art/`+ characterArray[index].artist.toLowerCase() +`.webp" class="front-img">`;
+        }
+
+        let twitter = ""; // Check if there's a Twitter
+        if (characterArray[index].twitter !== '') {
+            twitter += `<a href="https://www.twitter.com/${characterArray[index].twitter}"><div class="soclink twitter">${characterArray[index].twitter}</div></a>`; // If there is a Twitter, display it
+        } else {
+            twitter += ``;
+        }
+    
+        let tumblr = ""; // Check if there's a Tumblr
+        if (characterArray[index].tumblr !== '') {
+            tumblr += `<a href="https://${characterArray[index].tumblr}.tumblr.com/"><div class="soclink tumblr">${characterArray[index].tumblr}</div></a>`; // If there is a Tumblr, display it
+        } else {
+            tumblr += ``;
+        }
+    
+        let instagram = ""; // Check if there's a Instagram
+        if (characterArray[index].instagram !== '') {
+            instagram += `<a href="https://www.instagram.com/${characterArray[index].instagram}"><div class="soclink instagram">${characterArray[index].instagram}</div></a>`; // If there is a Instagram, display it
+        } else {
+            instagram += ``;
+        }
+    
+        let website = ""; // Check if there's a website
+        if (characterArray[index].website !== '') {
+            website += `<a href="${characterArray[index].website}"><div class="soclink website">${characterArray[index].websitename}</div></a>`; // If there is a website, display it
+        } else {
+            website += ``;
+        }
+
+        box.innerHTML = 
+        art + chibiart +
+        `<div class="modal-textbox"><div class="modal-title"> <div class="chara-title">` + title + `</div> / `+ characterArray[index].artist + `</div><div class="comment">` + description + `</div></div><div class="soclink-box">`+ twitter + tumblr + instagram + website +`</div>`;
+      });
+ 
+    });
+
+      return;
+    }
+
+    function artistListing() {
+
+            // Get data.csv and turn it into something readable
+    fetch('data.csv')
+    .then(response => response.text())
+    .then(characterCsv => {
+
+    // Read data.csv and create the array "characterArray" which creates objects for each rows in the array with these listed properties
+    const rows = characterCsv.split('\n');
+    const characterArray = rows.map(row => {
+    const values = row.split(',');
+    return {
+        id: values[0],
+        character: values[1],
+        chibi: values[2],
+        alt: values[3],
+        artist: values[4],
+        twitter: values[5],
+        tumblr: values[6],
+        instagram: values[7],
+        website: values[8],
+        websitename: values[9],
+        description: values[10]
+    };
+    });
+    console.log(characterArray);
+
+        // Get the artist list and create as many rows as there are rows in data.csv
+        let artists = document.getElementById('artists-parent');
+        
+        for (let i = 0; i < characterArray.length; i++) {
+            let artistsDiv = document.createElement('div');
+            artistsDiv.className = 'artist';
+            artists.appendChild(artistsDiv);
+        }
+
+        // ARTIST LIST
+        const artistList = document.querySelectorAll(".artist");
+        artistList.forEach((box, index) => {
+          let twitter = ""; // Check if there's a Twitter
+          if (characterArray[index].twitter !== '') {
+              twitter += `<a href="https://www.twitter.com/${characterArray[index].twitter}"><div class="soclink twitter">${characterArray[index].twitter}</div></a>`; // If there is a Twitter, display it
+          } else {
+              twitter += ``;
+          }  
+      
+          let tumblr = ""; // Check if there's a Tumblr
+          if (characterArray[index].tumblr !== '') {
+              tumblr += `<a href="https://${characterArray[index].tumblr}.tumblr.com/"><div class="soclink tumblr">${characterArray[index].tumblr}</div></a>`; // If there is a Tumblr, display it
+          } else {
+              tumblr += ``;
+          }
+      
+          let instagram = ""; // Check if there's a Instagram
+          if (characterArray[index].instagram !== '') {
+              instagram += `<a href="https://www.instagram.com/${characterArray[index].instagram}"><div class="soclink instagram">${characterArray[index].instagram}</div></a>`; // If there is a Instagram, display it
+          } else {
+              instagram += ``;
+          }
+      
+          let website = ""; // Check if there's a website
+          if (characterArray[index].website !== '') {
+              website += `<a href="${characterArray[index].website}"><div class="soclink website">${characterArray[index].websitename}</div></a>`; // If there is a website, display it
+          } else {
+              website += ``;
+          }
+          box.innerHTML = 
+          characterArray[index].artist + ` (`+ characterArray[index].character + `)   <div class="soclink-box artist-list">`+ twitter + tumblr + instagram + website + `</div>`;
+        });   
+
+    });
+        return;
+    }
+    
+// Open the Modal
+function openMenu() {
+    document.getElementById("menupopup").style.display = "block";
+  }
+  
+  // Close the Modal
+  function closeMenu() {
+    document.getElementById("menupopup").style.display = "none";
+  }
